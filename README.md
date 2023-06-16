efc3d3a44952d7d8b879943680c43d14630ccffb
최지환 <54761791+choe-ji-hwan@users.noreply.github.com>
2023-06-16 14:49:35 +0900

    step: 1-6 컴포저블 재사용

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 compose practice
 
 tutorial [link](https://developer.android.com/courses/jetpack-compose/course)
+
+1. Jetpack Compose 기초
+    - 수정자는 빈 수정자가 할당되는 수정자 매개변수 포함이 좋다.
+      ```kotlin
+        @Composable
+   private fun MyApp(modifier: Modifier = Modifier)
+      ```
--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.Compose_practiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +19,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_practiceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Greeting(name = "Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            modifier = Modifier.padding(24.dp),
+            text = "Hello $name!",
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_practiceTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
