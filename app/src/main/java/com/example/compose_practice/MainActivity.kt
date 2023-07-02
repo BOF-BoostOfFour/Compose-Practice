0a26b2418b076284a5eb3683b1adcd4356e50048
Choe-Ji-Hwan <ghldtjd901@naver.com>
2023-07-02 15:49:59 +0900

    feat: 12. 앱의 스타일 지정 및 테마 설정 구현(API 33이라서, 적용되진 않음)

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose_practice
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.Compose_practiceTheme
@@ -89,7 +91,9 @@ fun Greeting(name: String) {
                 .weight(1f)
                 .padding(extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             ElevatedButton(onClick = { expanded = !expanded }) {
                 Text(if (expanded) "Show less" else "Show more")
@@ -127,6 +131,12 @@ fun OnBoardingScreen(
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
